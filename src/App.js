d2385a8589f6d74d6c671a72d584eb8234fd13ca
Ines Part <inespart@Iness-MacBook-Air.local>
2021-05-18 16:35:49 +0200

    Add header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,134 @@
 import './App.css';
-import logo from './logo.svg';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import blogging from './images/blogging.png';
+import designwiz from './images/designwiz-logo.png';
+
+// CSS-in-JS
+
+const navBarContainer = css`
+  display: flex;
+  margin: 0 250px;
+  padding: 30px 0;
+
+  img {
+    width: 200px;
+  }
+`;
+
+const navBar = css`
+  margin-left: auto;
+`;
+
+const heroStyle = css`
+  background-color: #6999be;
+  background-image: linear-gradient(167deg, #fff 53%, #fce8d1 0, #fce8d1 103%);
+  height: 100vh;
+  margin-top: 115px;
+
+  img {
+    width: 120%;
+    height: auto;
+  }
+`;
+
+const flexContainer = css`
+  display: flex;
+  margin: 40px 250px;
+`;
+
+const largeBox = css`
+  width: 60%;
+
+  p {
+    font-size: 18px;
+    color: #696663;
+    line-height: 28px;
+    margin: 25px 0 15px;
+  }
+
+  span {
+    color: #926f46;
+    font-size: 14px;
+  }
+`;
+
+const smallBox = css`
+  width: 40%;
+`;
+
+const buttonGreen = css`
+  background-color: #066756;
+  border: none;
+  border-radius: 5px;
+  font-weight: 600;
+  color: #fff;
+  text-transform: uppercase;
+  padding: 12px 17px;
+  margin-top: 10px;
+  font-size: 14px;
+  font-family: 'Inter', sans-serif;
+  cursor: pointer;
+
+  :hover {
+    background-color: #055c4d;
+    box-shadow: rgb(0 0 0 / 12%) 0px 7px 50px 0px;
+  }
+`;
+
+const buttonBrown = css`
+  background-color: #d5c0a7;
+  border: none;
+  border-radius: 5px;
+  font-weight: 600;
+  color: #fff;
+  text-transform: uppercase;
+  padding: 5px 11px;
+  margin-right: 10px;
+  font-size: 12px;
+  font-family: 'Inter', sans-serif;
+`;
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <header>
+        <section css={navBarContainer}>
+          <img src={designwiz} alt="Designwiz Logo" />
+          <div css={navBar}>
+            {' '}
+            How it works Our work About us Login GET STARTED
+          </div>
+        </section>
+        <section css={heroStyle}>
+          <div css={flexContainer}>
+            <div css={largeBox}>
+              <div>
+                <button css={buttonBrown}>New!</button>
+                <span>Front-end development with Webflow</span>
+              </div>
+              <h1>
+                We Are Your Reliable, Affordable & Experienced Startup Design
+                Partner!
+              </h1>
+              <p>
+                High-quality design work to help you grow your business.
+                <br />
+                Save time and money with an expert design partner.
+              </p>
+              <button css={buttonGreen}>Get Started</button>
+            </div>
+            <div css={smallBox}>
+              <img src={blogging} alt="Blogging" />
+            </div>
+          </div>
+        </section>
+      </header>
+
+      {/* <main></main>
+      <footer></footer> */}
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
+/** @jsxImportSource @emotion/react */
+import './index.css';
+import { css, Global } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global
+      styles={css`
+        body {
+          background-color: #fff;
+          color: #232221;
+          margin: 0;
+          font-family: 'Open Sans', sans-serif;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+        }
+
+        h1 {
+          font-size: 40px;
+          font-weight: 600;
+          margin-top: 10px;
+          line-height: 48px;
+          text-decoration: none;
+        }
+
+        code {
+          font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+            monospace;
+        }
+      `}
+    />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
